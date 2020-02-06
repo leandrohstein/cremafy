407b0ccf907fd66f8b08fc4a4de630353eff672c
Leandro Henrique Stein <leandro.stein@madeiramadeira.com.br>
2020-02-06 13:59:56 -0300

    ++fix Updated image database

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,11 +1,22 @@
 //# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 const cremaImages = [
-    'https://i.imgur.com/KVuX73x.jpg',
-    'https://i.imgur.com/lJCQ2WB.jpg',
-    'https://i.imgur.com/iDNjnYS.jpg',
-    'https://i.imgur.com/2detEbD.jpg',
-    'https://i.imgur.com/DMJUQ9u.jpg',
+    'https://i.imgur.com/HIAvWXJ.jpg',
+    'https://i.imgur.com/89erdzs.jpg',
+    'https://i.imgur.com/Ix6vgXl.jpg',
+    'https://i.imgur.com/YX9jirt.jpg',
+    'https://i.imgur.com/UsSAGAp.jpg',
+    'https://i.imgur.com/GsOBLq4.jpg',
+    'https://i.imgur.com/czZT3Lh.jpg',
+    'https://i.imgur.com/5nifPg1.jpg',
+    'https://i.imgur.com/mAgXGe5.jpg',
+    'https://i.imgur.com/uQDYUDA.jpg',
+    'https://i.imgur.com/0TcYC36.jpg',
+    'https://i.imgur.com/ykiRZmn.jpg',
+    'https://i.imgur.com/EDFHpZb.jpg',
+    'https://i.imgur.com/g7jRoQg.jpg',
+    'https://i.imgur.com/QQ2JVL3.jpg',
+    'https://i.imgur.com/vpUlBdL.jpg',
 ];
 
 chrome.runtime.onMessage.addListener(function(message, sender, senderResponse){
